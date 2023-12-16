cf3a5260b086b5b504c01307df0e653804fb6021
TouhedurRahman <touhedur.cse@gmail.com>
2023-12-16 10:34:55 +0600

    send product to cart functionality added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express();
 const cors = require('cors');
+
+const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yrcmf.mongodb.net/?retryWrites=true&w=majority`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -23,20 +25,44 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // Database collections
         const menuCollection = client.db('restaurant_db').collection('menu');
+        const reviewCollection = client.db('restaurant_db').collection('reviews');
+        const cartCollection = client.db('restaurant_db').collection('cart');
+
+        // get all menues from db
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        });
+
+        // get all reviews from db
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        });
+
+        // send data to cart
+        app.post('/cart', async (req, res) => {
+            const item = req.body;
+            console.log(item);
+            const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send("Database Successfully Connected!");
+    res.send("✅ Database Successfully Connected!");
 });
 
 app.listen(port, () => {
